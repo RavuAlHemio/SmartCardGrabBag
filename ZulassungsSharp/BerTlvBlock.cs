a12222e69be96e4a16432cb0fed5a1f370f10171
Ondřej Hošek <ondra.hosek@gmail.com>
2016-08-12 22:16:28 +0200

    commit ZulassungsSharp, a tool to read the Austrian vehicle reg chip card

---
+++ ZulassungsSharp/BerTlvBlock.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.IO;
+
+namespace ZulassungsSharp
+{
+    class BerTlvBlock
+    {
+        public enum TagClass : byte
+        {
+            Universal = 0x0,
+            Application = 0x1,
+            ContextSpecific = 0x2,
+            Private = 0x3
+        }
+
+        public TagClass Class { get; protected set; }
+        public bool Constructed { get; protected set; }
+        public long TagNumber { get; protected set; }
+
+        public ImmutableArray<byte> RawBytes { get; protected set; }
+        public ImmutableArray<BerTlvBlock> SubBlocks { get; protected set; }
+
+        protected BerTlvBlock()
+        {
+        }
+
+        public void Write(Stream stream)
+        {
+            WriteTag(stream);
+
+            if (Constructed)
+            {
+                using (var buf = new MemoryStream())
+                {
+                    foreach (BerTlvBlock block in SubBlocks)
+                    {
+                        block.Write(buf);
+                    }
+
+                    buf.Seek(0, SeekOrigin.Begin);
+                    WriteLength(stream, buf.Length);
+                    buf.CopyTo(stream);
+                }
+            }
+            else
+            {
+                WriteLength(stream, RawBytes.Length);
+                var bytes = new byte[RawBytes.Length];
+                RawBytes.CopyTo(bytes);
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+        protected void WriteTag(Stream stream)
+        {
+            // first byte: Cl Cl Co Tn Tn Tn Tn Tn
+            var classByte = (byte)((byte)Class << 6);
+            var constructedByte = (byte)(Constructed ? (1 << 5) : 0);
+
+            if (TagNumber < 31)
+            {
+                // Cl Cl Co Tn Tn Tn Tn Tn
+                var firstByte = (byte)(classByte | constructedByte | (byte)TagNumber);
+                stream.WriteByte(firstByte);
+            }
+            else
+            {
+                // Cl Cl Co 1 1 1 1 1
+                var firstByte = (byte)(classByte | constructedByte | 0x1F);
+                stream.WriteByte(firstByte);
+
+                Debug.Assert(TagNumber >= 0);
+
+                // next bytes: 1 Tn Tn Tn Tn Tn Tn Tn
+                // last byte: 0 Tn Tn Tn Tn Tn Tn Tn
+                // shortest possible encoding
+
+                long currentTagNumber = TagNumber;
+                var tagBytes = new List<byte>();
+                while (currentTagNumber > 0)
+                {
+                    tagBytes.Add((byte)(currentTagNumber & 0x7F));
+                    currentTagNumber >>= 7;
+                }
+
+                // reverse for big endian
+                tagBytes.Reverse();
+
+                // set top bit on all tag bytes except the last
+                for (int i = 0; i < tagBytes.Count - 1; ++i)
+                {
+                    tagBytes[i] |= (1 << 7);
+                }
+                stream.Write(tagBytes.ToArray(), 0, tagBytes.Count);
+            }
+        }
+
+        protected void WriteLength(Stream stream, long length)
+        {
+            Debug.Assert(length >= 0);
+
+            if (length < 0x80)
+            {
+                // short form!
+                var lengthByte = (byte)length;
+                stream.WriteByte(lengthByte);
+                return;
+            }
+
+            // long form:
+            // first byte: 1 C C C C C C C
+            // subsequent bytes: L L L L L L L L
+            // C is the number of subsequent bytes, must be less than 0b1111111
+
+            var lengthBytes = new List<byte>();
+            while (length > 0)
+            {
+                lengthBytes.Add((byte)(length & 0xFF));
+                length >>= 8;
+            }
+
+            // reverse to get big endian
+            lengthBytes.Reverse();
+
+            Debug.Assert(lengthBytes.Count < 0x7F);
+
+            var countByte = (byte) ((1 << 7) | lengthBytes.Count);
+            stream.WriteByte(countByte);
+            stream.Write(lengthBytes.ToArray(), 0, lengthBytes.Count);
+        }
+
+        public static BerTlvBlock CreateFromStream(Stream stream)
+        {
+            int firstByteInt = stream.ReadByte();
+            if (firstByteInt == -1)
+            {
+                // end of stream, nothing to create
+                return null;
+            }
+            byte firstByte = (byte) firstByteInt;
+
+            var block = new BerTlvBlock
+            {
+                Class = (TagClass)((firstByte >> 6) & 0x3),
+                Constructed = ((firstByte & (1 << 5)) != 0)
+            };
+            long tagNumber = (firstByte & 0x1F);
+            if (tagNumber == 0x1F)
+            {
+                // long form tag number...
+                tagNumber = 0;
+                for (;;)
+                {
+                    byte tagByte = ReadByteOrThrow(stream);
+                    tagNumber = checked(tagNumber << 7);
+                    tagNumber |= (byte)(tagByte & 0x7F);
+
+                    // top bit 1 = more bytes follow
+                    if ((tagByte & 0x80) == 0)
+                    {
+                        // enough
+                        break;
+                    }
+                }
+            }
+            block.TagNumber = tagNumber;
+
+            long length = ReadByteOrThrow(stream);
+            if (length == 0x80)
+            {
+                throw new NotImplementedException("indefinite lengths are not supported");
+            }
+            else if (length > 0x80)
+            {
+                // multi-byte length
+                int lengthBytes = (int)(length & 0x7F);
+                length = 0;
+                for (int i = 0; i < lengthBytes; ++i)
+                {
+                    length = checked(length << 8);
+                    length |= ReadByteOrThrow(stream);
+                }
+            }
+            
+            var bytes = new byte[checked((int)length)];
+            ReadOrThrow(stream, bytes, 0, bytes.Length);
+
+            if (block.Constructed)
+            {
+                var builder = ImmutableArray.CreateBuilder<BerTlvBlock>();
+                using (var subStream = new MemoryStream(bytes, writable: false))
+                {
+                    BerTlvBlock subBlock;
+                    for (;;)
+                    {
+                        subBlock = CreateFromStream(subStream);
+                        if (subBlock == null)
+                        {
+                            break;
+                        }
+                        builder.Add(subBlock);
+                    }
+                }
+                block.SubBlocks = builder.ToImmutable();
+            }
+            else
+            {
+                block.RawBytes = ImmutableArray.Create(bytes);
+            }
+
+            return block;
+        }
+
+        protected static byte ReadByteOrThrow(Stream stream)
+        {
+            int b = stream.ReadByte();
+            if (b == -1)
+            {
+                throw new EndOfStreamException();
+            }
+            return (byte) b;
+        }
+
+        protected static void ReadOrThrow(Stream stream, byte[] buf, int offset, int count)
+        {
+            while (count > 0)
+            {
+                int bytesRead = stream.Read(buf, offset, count);
+                if (bytesRead == 0)
+                {
+                    throw new EndOfStreamException();
+                }
+                offset += bytesRead;
+                count -= bytesRead;
+            }
+        }
+
+        [ContractInvariantMethod]
+        private void ClassInvariant()
+        {
+            Contract.Invariant(Enum.IsDefined(typeof(TagClass), Class));
+            Contract.Invariant(TagNumber >= 0);
+            Contract.Invariant(Constructed || SubBlocks.IsEmpty);
+            Contract.Invariant(!Constructed || RawBytes.IsEmpty);
+        }
+    }
+}
---
+++ ZulassungsSharp/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
+using PCSC;
+using PCSC.Iso7816;
+
+namespace ZulassungsSharp
+{
+    class Program
+    {
+        static byte[] ObtainData(IsoReader reader, ushort dataID)
+        {
+            var selectApdu = new CommandApdu(IsoCase.Case3Short, reader.ActiveProtocol)
+            {
+                CLA = 0x00,
+                Instruction = InstructionCode.SelectFile,
+                P1 = 0x02,  // specific elementary file under current data file
+                P2 = 0x0C,  // first record, ?? template
+                Data = new[]
+                {
+                    (byte)((dataID >> 8) & 0xFF),
+                    (byte)((dataID >> 0) & 0xFF)
+                }
+            };
+            var selectResponse = reader.Transmit(selectApdu);
+            if (selectResponse.StatusWord != 0x9000)
+            {
+                Console.Error.WriteLine("E002!");
+                return null;
+            }
+
+            var readApdu = new CommandApdu(IsoCase.Case2Short, reader.ActiveProtocol)
+            {
+                CLA = 0x00,
+                Instruction = InstructionCode.ReadBinary,
+                P1 = 0x00,
+                P2 = 0x00,
+                Le = 0x00
+            };
+
+            var buffer = new List<byte>();
+            for (byte offset = 0; offset < 16; ++offset)
+            {
+                readApdu.P1 = offset;
+                var readResponse = reader.Transmit(readApdu);
+                if (readResponse.StatusWord == 0x6B00)
+                {
+                    // enough
+                    break;
+                }
+
+                buffer.AddRange(readResponse.GetData());
+            }
+            return buffer.ToArray();
+        }
+
+        static void ReadFromSmartCard()
+        {
+            CommandApdu apdu;
+            Response response;
+            byte[] responseData;
+
+            var factory = new ContextFactory();
+            using (var ctx = factory.Establish(SCardScope.System))
+            {
+                string readerName = ctx.GetReaders().First();
+                using (var reader = new IsoReader(ctx, readerName, SCardShareMode.Exclusive, SCardProtocol.T1, releaseContextOnDispose: false))
+                {
+                    apdu = new CommandApdu(IsoCase.Case3Short, reader.ActiveProtocol)
+                    {
+                        CLA = 0x00,
+                        Instruction = InstructionCode.SelectFile,
+                        P1 = 0x04,  // directly by DF (data file) name
+                        P2 = 0x0C,  // first record, ?? template
+                        Data = new byte[]
+                        {
+                            0xA0, 0x00, 0x00, 0x04,
+                            // "VEVR-01"
+                            0x56, 0x45, 0x56, 0x52, 0x2D, 0x30, 0x31
+                        }
+                    };
+                    response = reader.Transmit(apdu);
+                    if (response.StatusWord != 0x9000)
+                    {
+                        Console.Error.WriteLine("E013a!");
+                        return;
+                    }
+
+                    apdu = new CommandApdu(IsoCase.Case2Short, reader.ActiveProtocol)
+                    {
+                        CLA = 0xA0,
+                        Instruction = InstructionCode.GetData,
+                        P1 = 0x9F,  // 2-byte BER-TLV tag
+                        P2 = 0x6A,  // 2-byte BER-TLV tag
+                        Le = 0x00
+                    };
+                    response = reader.Transmit(apdu);
+                    responseData = response.GetData();
+                    if (responseData[0] != 0x47 || responseData[1] != 0x44)
+                    {
+                        Console.Error.WriteLine("E013b!");
+                        return;
+                    }
+
+                    var sections = new ushort[] { 0xD001, 0xE001, 0xC001, 0xD011, 0xE011, 0xC011 };
+                    foreach (ushort section in sections)
+                    {
+                        using (var o = new FileStream($"{section:X4}.bin", FileMode.Create, FileAccess.Write,
+                            FileShare.None))
+                        {
+                            byte[] data = ObtainData(reader, section);
+                            o.Write(data, 0, data.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //ReadFromSmartCard();
+
+            var sections = new ushort[] {0x001, 0x011};
+            foreach (ushort section in sections)
+            {
+                ushort certificateSection = (ushort)(section | 0xC000);
+                ushort dataSection = (ushort) (section | 0xD000);
+                ushort signatureSection = (ushort) (section | 0xE000);
+
+                byte[] certificateBytes = File.ReadAllBytes($"{certificateSection:X4}.bin");
+                byte[] dataBytes = File.ReadAllBytes($"{dataSection:X4}.bin");
+                byte[] signatureBytes = File.ReadAllBytes($"{signatureSection:X4}.bin");
+
+                BerTlvBlock signatureObject;
+                using (var signatureStream = new MemoryStream(signatureBytes, writable: false))
+                {
+                    signatureObject = BerTlvBlock.CreateFromStream(signatureStream);
+                }
+
+                // ECDSA signature (as integer pair r, s) of SHA256 digest of data
+                var hasher = new Sha256Digest();
+                var hashBytes = new byte[hasher.GetDigestSize()];
+                hasher.BlockUpdate(dataBytes, 0, dataBytes.Length);
+                hasher.DoFinal(hashBytes, 0);
+
+                byte[] rBytes = signatureObject.SubBlocks[0].RawBytes.ToArray();
+                byte[] sBytes = signatureObject.SubBlocks[1].RawBytes.ToArray();
+                var r = new BigInteger(rBytes);
+                var s = new BigInteger(sBytes);
+
+                var cert = X509CertificateStructure.GetInstance(Asn1Object.FromByteArray(certificateBytes));
+                var pubkey = (ECPublicKeyParameters) PublicKeyFactory.CreateKey(cert.SubjectPublicKeyInfo);
+
+                var signer = new ECDsaSigner();
+                signer.Init(forSigning: false, parameters: pubkey);
+                if (signer.VerifySignature(hashBytes, r, s))
+                {
+                    Console.WriteLine("signature OK");
+                }
+                else
+                {
+                    Console.WriteLine("SIGNATURE BAD!!!");
+                }
+
+                var dataBlocks = new List<BerTlvBlock>();
+                using (var dataStream = new MemoryStream(dataBytes, writable: false))
+                {
+                    for (;;)
+                    {
+                        var block = BerTlvBlock.CreateFromStream(dataStream);
+                        if (block == null)
+                        {
+                            break;
+                        }
+                        dataBlocks.Add(block);
+                    }
+                }
+
+                foreach (var dataBlock in dataBlocks)
+                {
+                    OutputBlock(dataBlock);
+                }
+            }
+        }
+
+        static void OutputBlock(BerTlvBlock block, int depth = 0)
+        {
+            var indent = new string(' ', 2*depth);
+            if (block.Constructed)
+            {
+                Console.WriteLine($"{indent}{block.TagNumber}:");
+                foreach (var subBlock in block.SubBlocks)
+                {
+                    OutputBlock(subBlock, depth + 1);
+                }
+            }
+            else
+            {
+                var bytes = block.RawBytes.ToArray();
+                var encoding = Encoding.GetEncoding("windows-1252");
+                var text = encoding.GetString(bytes);
+                Console.WriteLine($"{indent}{block.TagNumber}: {text}");
+            }
+        }
+    }
+}
